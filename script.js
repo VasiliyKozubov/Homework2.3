c110e6b513815b7452b04b4f5f2958df35f81d49
Vasiliy_Kozubov` <kozubov27@gmail.com`>
2023-04-01 12:19:45 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,81 @@
+// let password = String('qwe123');
+// let enterpassword = String(prompt('Введите пароль'));
+
+// if (password === enterpassword) {
+//     alert('Пароль введен верно');
+// } else {
+//     alert('Пароль введен неправильно');
+// }
+
+
+
+// let c = 5;
+// if (c > 0 && c < 10) {
+//     console.log('верно');
+// } else {
+//     console.log('неверно');
+// }
+
+
+
+// let d = 105;
+// let e = 56;
+// if (d > 100 || e> 100) {
+//     console.log('Верно');
+// } else {
+//     console.log('Неверно');
+// }
+
+
+
+
+// let a = Number('2');
+// let b = Number('3');
+// alert(a + b);
+
+
+
+// let monthNumber = String(prompt('Введите номер месяца что-бы узнать к какому сезону он относится'))
+
+// switch (monthNumber) {
+//     case '1':
+//         console.log('Зима')
+//         break;
+//     case '2':
+//         console.log('Зима')
+//         break;
+//     case '3':
+//         console.log('Весна')
+//         break;
+//     case '4':
+//         console.log('Весна')
+//         break;
+//     case '5':
+//         console.log('Весна')
+//         break;
+//     case '6':
+//         console.log('Лето')
+//         break;
+//     case '7':
+//         console.log('Лето')
+//         break;
+//     case '8':
+//         console.log('Лето')
+//         break;
+//     case '9':
+//         console.log('Осень')
+//         break;
+//     case '10':
+//         console.log('Осень')
+//         break;
+//     case '11':
+//         console.log('Осень')
+//         break;
+//     case '12':
+//         console.log('Зима')
+//         break;
+
+//     default:
+//         console.log('Такого месяца не существует')
+//         break;
+// }
